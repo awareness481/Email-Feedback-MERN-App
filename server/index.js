9323e2f848e0a399edd41e71eb213fe299fd1ae1
Jesse Jafa <jesse.jafa@gmail.com>
2018-09-22 08:54:49 +0300

    Initial Mongo/Mongoose setup, creating a new User

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+const keys = require("./config/keys");
+require('./models/User');
 require('./services/passport');
 
-
+mongoose.connect(keys.mongoURI);
 const app = express();
 require('./routes/authRoutes')(app);
 
