f71ed17cfe60949e8241dd1d0f0cdc508f1871b7
Jesse Jafa <jesse.jafa@gmail.com>
2018-10-03 14:37:47 +0300

    Correct header content for null/false/{} authReducer

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+
+  renderContect() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href='/auth/google'>Login with Google</a></li>
+        );
+      default:
+        return (
+          <li><a>Log out</a></li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className='nav-wrapper'>
           <a className='left brand-logo'>Emaily</a>
           <ul className='right'>
-            <li><a>Login with Google</a></li>
+            {this.renderContect()}
           </ul>
         </div>
       </nav>
@@ -15,4 +32,8 @@ class Header extends Component {
   }
 };
 
-export default Header;
+function mapStateToProps({auth}) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
