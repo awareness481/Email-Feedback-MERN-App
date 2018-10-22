91b4b3453656dc6a69aebe30b5e2b765569149d3
Jesse Jafa <jesse.jafa@gmail.com>
2018-10-22 06:55:24 +0300

    Added credits to the user model, adjusted billingRoutes to add credits to user on charge

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -5,11 +5,22 @@ const stripe = require('stripe')
 module.exports = (app) => {
   app.post(
     '/api/stripe', async (req, res) => {
+      if (!req.user) {
+        return res.status(401).send({
+          error: 'You must log in'
+        });
+      }
+
       const charge = await stripe.charges.create({
         amount: 500,
         currency: 'usd',
         source: req.body.id,
         description: '$5 for 5 credits',
       });
+
+      req.user.credits += 5;
+      const user = await req.user.save();
+
+      res.send(user);
     });
 };
