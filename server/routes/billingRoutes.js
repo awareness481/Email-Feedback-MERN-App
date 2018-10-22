5078230086ea41d13fa1a8531cb872d01dfdaa32
Jesse Jafa <jesse.jafa@gmail.com>
2018-10-22 07:08:04 +0300

    Refactored login requirement to requireLogin.js

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,15 +1,11 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')
   (keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = (app) => {
   app.post(
-    '/api/stripe', async (req, res) => {
-      if (!req.user) {
-        return res.status(401).send({
-          error: 'You must log in'
-        });
-      }
+    '/api/stripe', requireLogin, async (req, res) => {
 
       const charge = await stripe.charges.create({
         amount: 500,
