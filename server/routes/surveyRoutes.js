feca70bf62db750fea881e0f3d1c1c87fc12ea13
Jesse Jafa <jesse.jafa@gmail.com>
2018-10-24 14:56:01 +0300

    added more properties

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -8,5 +8,14 @@ module.exports = (app) => {
   app.post(
     '/api/surveys', requireLogin, requireCredits, (req, res) => {     
       const { title, subject, body, recipients } = req.body;
+
+      const survey = new Survey({
+        title,
+        body,
+        subject,
+        recipients: recipients.split(',').map(email => ({ email })),
+        _user: req.user.id,
+        dateSent: Date.now()
+      })
   })
 };
