f380534b01f2c4e06dbf805a9648d3d59c64f6fb
Jesse Jafa <jesse.jafa@gmail.com>
2018-10-25 12:23:22 +0300

    Improved promise handling

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -8,6 +8,10 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('Thanks for voting!');
+  })
+
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
@@ -21,6 +25,18 @@ module.exports = app => {
     });
 
       const mailer = new Mailer(survey, surveyTemplate(survey));
-      mailer.send();
+      
+      try {
+        await mailer.send();
+
+        await survey.save();
+        req.user.credits -= 1;
+        const user = await req.user.save();
+  
+        res.send(user);
+      } catch (err) {
+        res.status(422).send(err);
+      }
+
   });
 };
