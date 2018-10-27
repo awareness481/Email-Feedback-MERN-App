8e97b86243cc81df70506c4e9b46db9f325a38b8
Jesse Jafa <jesse.jafa@gmail.com>
2018-10-27 10:02:23 +0300

    Added form validation & DRY <Field />

--- server/client/src/components/Surveys/SurveyField.js
+++ server/client/src/components/Surveys/SurveyField.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export default ({ input }) => {
+export default ({ input, label, meta: {error, touched} }) => {
   return (
     <div>
-      <input {...input}/>
+      <label>{label}</label>
+      <input style={{marginBottom: '5px'}} {...input}/>
+      <div className='red-text' style={{marginBottom: '20px'}}>
+        {touched && error}
+      </div>
     </div>
   );
 }
--- server/client/src/components/Surveys/SurveyForm.js
+++ server/client/src/components/Surveys/SurveyForm.js
@@ -1,16 +1,22 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 import SurveyField from './SurveyField';
 
+const FIELDS = [
+  {label: 'SurveyTitle', name :'title'},
+  {label: 'Subject Line', name: 'subject'},
+  {label: 'Email Body', name: 'body'},
+  {label: 'Recipients List', name: 'emails'}
+]
 
 class SurveyForm extends Component {
   renderFields() {
-    return (
-      <div>
-        <Field type='text' name='title' component={SurveyField} />
-      </div>
-    );
+    return _.map(FIELDS, ({label, name}) => {
+      return <Field key={name} component={SurveyField} type='text' label={label} name={name} />
+    });
   }
 
   render() {
@@ -19,13 +25,33 @@ class SurveyForm extends Component {
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}
         >
           {this.renderFields()}
-          <button type='submit'>Submit</button>
+          <Link to='/surveys' className='red btn-flat white-text'>
+            Cancel
+          </Link>
+          <button 
+            type='submit'
+            className='teal btn-flat right white-text'>
+            Next
+            <i className='material-icons right'>done</i>
+          </button>
         </form>
       </div>
     );
   }
 }
 
+function validate(values) {
+  // If errors object is empty form is valid
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'You must provide a title';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'surveyForm'
 })(SurveyForm);
