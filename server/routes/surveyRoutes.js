fe8453d99bbce9b9ecb8053efd6f9bb64d4a9c8d
Jesse Jafa <jesse.jafa@gmail.com>
2018-11-11 10:04:07 +0200

    added route login to display list of surveys on /surveys

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -14,7 +14,14 @@ const Survey = mongoose.model('surveys');
 module.exports = app => {
   app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
-  })
+  });
+
+  app.get('/api/surveys/', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id })
+      .select({ 'recipients': false });
+
+    res.send(surveys);
+  });
 
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyId/:choice');
