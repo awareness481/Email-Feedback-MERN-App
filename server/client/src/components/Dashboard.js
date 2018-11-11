45d21a7471619cf8cb51b9b50f3ba1ccdf78002d
Jesse Jafa <jesse.jafa@gmail.com>
2018-11-11 10:04:40 +0200

    Added react, redux implementation to display surveys on the frontend

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import SurveyList from './Surveys/SurveyList';
+
 const Dashboard = () => {
   return (
     <div>
-      Dashboard
+      <SurveyList />
       <div className='fixed-action-btn'>
         <Link to='/surveys/new' class='btn-floating btn-large red'>
           <i className='material-icons'>add</i>
---
+++ server/client/src/components/Surveys/SurveyList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchSurveys } from '../../actions';
+
+class SurveyList extends Component {
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
+
+  renderSurveys() {
+    return this.props.surveys.map(survey => {
+      return (
+        <div className="card blue-grey darken-1" key={survey._id}>
+          <div className="card-content white-text">
+            <span class="card-title">{survey.title}</span>
+            <p>{survey.body}</p>
+            <p className='right'>Sent on: {new Date(survey.dateSent).toLocaleDateString()}</p>
+          </div>
+        <div className='card-action'>
+          <a href='#'>Yes: {survey.yes}</a>
+          <a href='#'>No: {survey.no}</a>
+        </div>
+      </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderSurveys()}
+      </div>
+    );
+  }
+};
+
+function mapStateToProps({ surveys }) {
+  return { surveys };
+};
+
+export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
